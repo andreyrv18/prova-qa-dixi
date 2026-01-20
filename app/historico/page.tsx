0b30ef936a03f7dacdd63acfccf7d528d6f77b86
Andreyrvdev <andreyrv18@gmail.com>
2026-01-20 00:45:55 -0300

    feat: adiciona layout na pagina de histórico

--- app/historico/page.tsx
+++ app/historico/page.tsx
@@ -1,13 +1,45 @@
 import SideBar from '@/app/ui/sideBar';
 import Card from '@/app/ui/card';
+import { Input } from '@/app/ui/input';
+import { Button } from '@/app/ui/button';
+import { Hr } from '@/app/ui/hr';
 
 export default function Historico() {
     return (
-        <section className="grid grid-cols-[auto_1fr]">
+        <section className="grid h-screen grid-cols-[auto_1fr]">
             <SideBar />
-            <Card>
-                <h1>Historico</h1>
-            </Card>
+            <section className="flex h-full w-full flex-col items-center justify-center">
+                <div className="flex w-fit flex-col items-start gap-5">
+                    <header className="flex flex-col items-start pl-4">
+                        <h1 className="text-titulo-pagina text-azul-base font-bold">
+                            Historico
+                        </h1>
+                        <h2 className="text-subtitulo text-corpo-de-texto font-semibold">
+                            Veja os Pontos Registrados no Sistema
+                        </h2>
+                    </header>
+                    <main>
+                        <Card>
+                            <div>tes</div>
+                            <div className="flex flex-col justify-center gap-2">
+                                <div className="flex flex-row items-center justify-center">
+                                    <Input label="Data Inicial"></Input>
+
+                                    <Input label="Data Final"></Input>
+                                    <Button buttonSize="large" icon={true}>
+                                        Pesquisar
+                                    </Button>
+                                </div>
+                                <div className="flex flex-row items-center justify-center">
+                                    <p>d</p>
+                                    <p>t</p>
+                                </div>
+                            </div>
+                            <Hr />
+                        </Card>
+                    </main>
+                </div>
+            </section>
         </section>
     );
 }

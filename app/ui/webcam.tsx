c4014da94cdd76a4b09bd7ff49208eb16051f4dc
Andreyrvdev <andreyrv18@gmail.com>
2026-01-21 01:33:36 -0300

    fix: componente de webcam movendo os itens do viewFinder e adicionando o dispatch de eventos e o stado da webcam. Lida com a logica da webcam desativada exibindo um card com text "Foto desabilitada"

--- app/ui/webcam.tsx
+++ app/ui/webcam.tsx
@@ -1,6 +1,11 @@
 'use client';
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useContext, useRef } from 'react';
 import Webcam from 'react-webcam';
+import {
+    WebcamContext,
+    WebcamDispatchContext,
+} from '@/app/context/WebcamContext';
+import ViewFinder from '@/app/ui/viewFinder';
 
 const videoConstraints = {
     width: 360,
@@ -8,30 +13,33 @@ const videoConstraints = {
     facingMode: 'environment',
 };
 
-interface WebcamCaptureProps {
-    onCapture: (imageSrc: string) => void;
-}
-
-export default function WebcamComponent({ onCapture }: WebcamCaptureProps) {
+export default function WebcamComponent() {
+    const dispatch = useContext(WebcamDispatchContext);
+    const webcamState = useContext(WebcamContext);
     const webcamRef = useRef<Webcam>(null);
-
-    const [deactivate, setDeactivate] = useState<boolean>(false);
-    const toggle = () => setDeactivate((prevState) => !prevState);
 
     const capturePhoto = useCallback(() => {
         if (webcamRef.current) {
             const imageSrc: string | null = webcamRef.current.getScreenshot();
             if (imageSrc) {
-                onCapture(imageSrc);
+                dispatch?.({ type: 'SET_IMAGE', payload: imageSrc });
             }
         }
-    }, [onCapture]);
+    }, [webcamRef, dispatch]);
+
+    if (!webcamState?.isWebcamOpen) {
+        return (
+            <div className="bg-background-2 text-corpo-de-texto text-interior-campo-texto flex h-[620] w-[620] items-center justify-center overflow-hidden rounded-2xl font-semibold">
+                Foto desabilitada
+            </div>
+        );
+    }
 
     return (
         <>
             <div className="relative flex h-[620] w-[620] items-center justify-center">
                 <Webcam
-                    className={`${deactivate ? 'hidden' : ''} rounded-l-xl`}
+                    className={`${webcamState?.deactivatedWebcam ? 'hidden' : ''} rounded-l-xl`}
                     ref={webcamRef}
                     audio={false}
                     height={620}
@@ -40,15 +48,8 @@ export default function WebcamComponent({ onCapture }: WebcamCaptureProps) {
                     videoConstraints={videoConstraints}
                     screenshotQuality={1}
                 />
-                <span className="border-background absolute top-18 left-18 h-24 w-24 rounded-tl-[50] border-t-6 border-l-6"></span>
-                <span className="border-background absolute top-14 right-14 h-28 w-28 rounded-tr-[60] border-t-6 border-r-6"></span>
-                <span className="border-background absolute right-14 bottom-14 h-28 w-28 rounded-br-[60] border-r-6 border-b-6"></span>
-                <span className="border-background absolute bottom-14 left-14 h-28 w-28 rounded-bl-[60] border-b-6 border-l-6"></span>
-                <div className="text-background absolute inset-2 rounded-lg p-4 text-center">
-                    <p className="inferior-campo-texto text-background">
-                        Centralize o rosto na moldura para tirar a foto.
-                    </p>
-                </div>
+
+                <ViewFinder />
             </div>
         </>
     );

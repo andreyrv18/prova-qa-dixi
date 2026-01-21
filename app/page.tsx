cf0d804a4d8e7677f6e90052c95f0230683fd03d
Andreyrvdev <andreyrv18@gmail.com>
2026-01-21 01:31:47 -0300

    feat: Utiliza context e dispatch da webcam sendo usado no switch para logica de habilitar ou desabilitar o compente de webcam e do modal sendo usado no botao Registrar Ponto

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,23 @@ import WebcamComponent from '@/app/ui/webcam';
 import Card from '@/app/ui/card';
 import Switch from '@/app/ui/switch';
 import Modal from '@/app/ui/modal';
-import { useState } from 'react';
+import { useContext } from 'react';
 import Image from 'next/image';
-import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import { Button } from '@/app/ui/button';
+import {
+    WebcamContext,
+    WebcamDispatchContext,
+} from '@/app/context/WebcamContext';
+import { ModalContext, ModalDispatchContext } from '@/app/context/ModalContext';
+import Relogio from '@/app/ui/relogio';
 
 export default function Home() {
-    const [capturedImage, setCapturedImage] = useState<string | StaticImport>(
-        '',
-    );
-    const [showModal, setShowModal] = useState(false);
-
-    const handleCapture = (imageSrc: string) => {
-        setCapturedImage(imageSrc);
-        setShowModal(true);
-    };
+    const webcamState = useContext(WebcamContext);
+    const webcamDispatch = useContext(WebcamDispatchContext);
+    const modalState = useContext(ModalContext);
+    const modalDispatch = useContext(ModalDispatchContext);
     return (
-        <div className="grid h-screen grid-cols-[auto_1fr]">
+        <div className="bg-background grid h-screen grid-cols-[auto_1fr]">
             <SideBar />
             <section className="flex h-full w-full flex-col items-center justify-center">
                 <div className="flex w-fit flex-col items-start gap-5">
@@ -34,37 +34,31 @@ export default function Home() {
                     </header>
                     <main className="">
                         <Card>
-                            <WebcamComponent onCapture={handleCapture} />
+                            <WebcamComponent />
                             <div className="flex w-full flex-col justify-between p-3">
-                                <div className="flex flex-col gap-4">
-                                    <h2 className="text-subtitulo text-azul-base font-bold">
-                                        Terça-Feira
-                                    </h2>
-
-                                    <h1 className="text-azul-base text-6xl font-bold">
-                                        12:32
-                                        <span className="text-subtitulo text-corpo-de-texto font-semibold">
-                                            32
-                                        </span>
-                                    </h1>
-                                    <p className="text-azul-base text-subtitulo font-semibold">
-                                        21/02/2026
-                                    </p>
-                                    <p className="text-corpo-de-texto">
-                                        A data e hora serão registrados no
-                                        sistema ao realizar a marcação.
-                                    </p>
+                                <Relogio>
                                     <Switch
-                                        // onClick={}
                                         label="Tirar foto para bater ponto"
+                                        onChange={() => {
+                                            webcamDispatch?.({
+                                                type: 'OPEN_WEBCAM',
+                                            });
+                                        }}
+                                        checked={
+                                            webcamState?.isWebcamOpen ?? false
+                                        }
                                     ></Switch>
-                                </div>
+                                </Relogio>
                                 <div className="flex flex-col gap-6">
                                     <Button
                                         className="items-center"
                                         icon={true}
                                         buttonSize="large"
-                                        // onClick={capturePhoto}
+                                        onClick={() =>
+                                            modalDispatch?.({
+                                                type: 'OPEN_MODAL',
+                                            })
+                                        }
                                     >
                                         Registrar Ponto
                                     </Button>
@@ -74,22 +68,28 @@ export default function Home() {
                     </main>
                 </div>
                 <Modal
-                    openModal={showModal}
-                    closeModal={() => setShowModal(false)}
+                    isModalOpen={modalState?.isModalOpen ?? false}
+                    onModalClose={() =>
+                        modalDispatch?.({
+                            type: 'CLOSE_MODAL',
+                        })
+                    }
                 >
                     <section className="grid-cols[1fr_1fr] grid">
                         <h1 className="text-titulo-modal text-azul-base font-bold">
                             Prévia da Marcação
                         </h1>
 
-                        {capturedImage && (
+                        {webcamState?.capturedImage ? (
                             <Image
-                                src={capturedImage}
+                                src={webcamState.capturedImage}
                                 width={300}
                                 height={300}
                                 className="rounded-md object-cover"
                                 alt="Foto capturada da Webcam"
                             ></Image>
+                        ) : (
+                            <p>Sem Foto</p>
                         )}
                     </section>
                 </Modal>

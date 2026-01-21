7957ba0542e39dcfaf530da68074759ff9d573aa
Andreyrvdev <andreyrv18@gmail.com>
2026-01-21 01:25:55 -0300

    fix: cores do card para corresponder a UI

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -8,7 +8,7 @@ export default function Card({ children, ...rest }: CardBarProps) {
     return (
         <section
             {...rest}
-            className="border-outline bg-background-2 grid h-[620] w-[932] grid-cols-[auto_1fr] gap-2 rounded-2xl border-2 shadow-xl"
+            className="border-outline bg-background grid h-[620] w-[932] grid-cols-[auto_1fr] gap-2 rounded-2xl border-2 shadow-xl"
         >
             {children}
         </section>

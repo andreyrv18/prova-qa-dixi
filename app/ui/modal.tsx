1b8c35f5337b48eaf7db0843d7df1125517f63f9
Andreyrvdev <andreyrv18@gmail.com>
2026-01-21 01:29:37 -0300

    fix: modal para receber o context ao abrir ou fechar o modal

--- app/ui/modal.tsx
+++ app/ui/modal.tsx
@@ -1,35 +1,39 @@
-import React, { useEffect, useRef } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 import { FaXmark } from 'react-icons/fa6';
 
 interface ModalProps {
-    children: React.ReactNode;
-    openModal: boolean;
-    closeModal: () => void;
+    children: ReactNode;
+    isModalOpen: boolean;
+    onModalClose: () => void;
 }
 
-export default function Modal({ openModal, closeModal, children }: ModalProps) {
+export default function Modal({
+    isModalOpen,
+    onModalClose,
+    children,
+}: ModalProps) {
     const ref = useRef<HTMLDialogElement>(null);
 
     useEffect(() => {
         if (!ref.current) return;
 
-        if (openModal) {
+        if (isModalOpen) {
             ref.current?.showModal();
         } else {
             ref.current?.close();
         }
-    }, [openModal]);
+    }, [isModalOpen]);
 
     return (
         <>
             <dialog
                 ref={ref}
-                onCancel={closeModal}
+                onCancel={onModalClose}
                 className="open:animate-in open:fade-in m-auto h-[446] w-[672] rounded-lg p-6 shadow-xl backdrop:bg-black/50"
             >
                 {children}
                 <button
-                    onClick={closeModal}
+                    onClick={onModalClose}
                     className="border-azul-base hover:border-azul-hover hover:bg-azul-hover active:bg-azul-base active:border-azul-base active:text-background absolute top-6 right-5 flex h-6 w-6 items-center justify-center rounded-sm border-2"
                 >
                     <FaXmark className="text-azul-base active:text-background" />

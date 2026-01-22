91ba1009d37ff94dfa1f6aa5d76f5d9e8858d4d6
Andreyrvdev <andreyrv18@gmail.com>
2026-01-22 00:26:32 -0300

    fix: altera como o buttom exibe os Icones

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,12 +1,11 @@
 'use client';
-import React from 'react';
-import { FaPlus } from 'react-icons/fa6';
+import React, { ReactElement } from 'react';
+import { IconBaseProps } from 'react-icons';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
     buttonSize: string;
-    icon?: boolean;
-
+    icon: ReactElement<IconBaseProps> | boolean;
     forceHover?: boolean;
     forceActive?: boolean;
     forceTranparent?: boolean;
@@ -22,13 +21,12 @@ export function Button({
     forceHover,
     forceTranparent,
     forceHoverFill,
-    ...rest
+    ...props
 }: ButtonProps) {
     const hasButtonSize: string =
         buttonSize == 'pequeno'
             ? 'rounded-3xl w-38 h-8'
             : 'rounded-md w-38 h-8';
-    const hasIcon = icon ? <FaPlus></FaPlus> : null;
 
     const buttonActive =
         'border-bg-[#3379bc] bg-[#3379bc] text-background pointer-events-none';
@@ -37,6 +35,7 @@ export function Button({
         'border-none  border-transparent bg-transparent text-transparent  pointer-events-none';
     const buttonHoverFill =
         'bg-azul-leve border-azul-hover  bg-azul-hover text-background  pointer-events-none';
+
     if (forceActive) {
         className += buttonActive;
     } else if (forceHover) {
@@ -45,14 +44,16 @@ export function Button({
         className += buttonTransparent;
     } else if (forceHoverFill) {
         className += buttonHoverFill;
+    } else if (hasButtonSize) {
+        className += hasButtonSize;
     }
 
     return (
         <button
-            {...rest}
-            className={`${hasButtonSize} ${className} text-md border-azul-base hover:bg-azul-leve active:border-bg-[#3379bc] active:text-background flex cursor-pointer items-center justify-center border active:bg-[#3379bc]`}
+            {...props}
+            className={`${hasButtonSize} ${className} text-md border-azul-base hover:bg-azul-leve active:border-bg-[#3379bc] active:text-background flex cursor-pointer items-center justify-center gap-2 border active:bg-[#3379bc]`}
         >
-            {hasIcon}
+            <>{icon}</>
             {children}
         </button>
     );

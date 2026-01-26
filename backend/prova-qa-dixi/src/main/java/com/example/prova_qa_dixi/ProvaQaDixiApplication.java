ebd017d9213d3f2fdc4c605d8c7c08164c8fa5cf
Andreyrvdev <andreyrv18@gmail.com>
2026-01-25 23:59:22 -0300

    feat(Backend): arquivo Main

---
+++ backend/prova-qa-dixi/src/main/java/com/example/prova_qa_dixi/ProvaQaDixiApplication.java
@@ -0,0 +1,13 @@
+package com.example.prova_qa_dixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaQaDixiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaQaDixiApplication.class, args);
+	}
+
+}

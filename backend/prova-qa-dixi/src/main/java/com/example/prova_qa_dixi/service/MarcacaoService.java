5e526e38a5dc086ed0f5ecab9e950b0d8d23ca99
Andreyrvdev <andreyrv18@gmail.com>
2026-01-25 23:56:32 -0300

    Feature(Backend): Service Marcação

---
+++ backend/prova-qa-dixi/src/main/java/com/example/prova_qa_dixi/service/MarcacaoService.java
@@ -0,0 +1,19 @@
+package com.example.prova_qa_dixi.service;
+
+import com.example.prova_qa_dixi.model.Marcacao;
+import com.example.prova_qa_dixi.repository.MarcacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MarcacaoService {
+
+
+    private MarcacaoRepository marcacaoRepository;
+
+    public Marcacao marcacaoService(Marcacao marcacao) {
+        System.out.println(marcacao);
+      return   marcacaoRepository.save(marcacao);
+
+    }
+}

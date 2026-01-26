a1e1621c731133f05a697fedf8d72db6d55714e5
Andreyrvdev <andreyrv18@gmail.com>
2026-01-26 01:27:43 -0300

    feat(Frontend): Adicionar chamada Get da API para pegar a data e hora do servidor. Utilizar URl separada para cada requisicao.

--- frontend/app/services/fetchAPI.ts
+++ frontend/app/services/fetchAPI.ts
@@ -1,13 +1,16 @@
-const URL = 'http://localhost:8080/marcacoes';
 const CONTENT = 'application/json';
 
 export const fetchAPI = async () => {
+    const URL = 'http://localhost:8080/';
+
     const response = await fetch(URL);
 
     return await response.json();
 };
 
 export const PostMarcacoes = async (payload: object) => {
+    const URL = 'http://localhost:8080/marcacoes';
+
     const response = await fetch(URL, {
         method: 'POST',
         headers: {
@@ -19,4 +22,17 @@ export const PostMarcacoes = async (payload: object) => {
     const { status } = response;
     const data = await response.json();
     return { data, status };
+};
+
+export const GetServerTime = async () => {
+    const URL = 'http://localhost:8080/time/sync';
+    const response = await fetch(URL, { cache: 'no-cache' });
+
+    if (!response.ok) {
+        console.error('Falha na rede');
+    }
+
+    const data = await response.json();
+
+    return Number(data['server time']);
 };

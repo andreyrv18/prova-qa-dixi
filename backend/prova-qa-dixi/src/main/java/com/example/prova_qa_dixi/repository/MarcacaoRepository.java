5c779dc0e1d712e288d9cf8575d10db7d7b6a71d
Andreyrvdev <andreyrv18@gmail.com>
2026-01-25 23:56:08 -0300

    Feature(Backend): Repository Marcação para fazer consultas no Banco de dados

---
+++ backend/prova-qa-dixi/src/main/java/com/example/prova_qa_dixi/repository/MarcacaoRepository.java
@@ -0,0 +1,10 @@
+package com.example.prova_qa_dixi.repository;
+
+import com.example.prova_qa_dixi.model.Marcacao;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MarcacaoRepository extends CrudRepository<Marcacao, Long> {
+
+    Marcacao findAllById(Long id);
+}
+

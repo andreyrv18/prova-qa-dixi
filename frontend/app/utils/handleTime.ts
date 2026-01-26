8e6837ca67f65a4262d9acf2a8be16bbfc4aa167
Andreyrvdev <andreyrv18@gmail.com>
2026-01-26 09:44:50 -0300

    fix(Frontend): separa logica de data e fuso horario movendo para um arquivo handleTime.ts,

---
+++ frontend/app/utils/handleTime.ts
@@ -0,0 +1,41 @@
+const TIME_ZONE = 'America/Sao_Paulo';
+
+const opcoesTempo: Intl.DateTimeFormatOptions = {
+    timeZone: TIME_ZONE,
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+};
+
+const opcoesSegundo: Intl.DateTimeFormatOptions = {
+    timeZone: TIME_ZONE,
+    second: '2-digit',
+};
+
+const opcoesDiaDaSemana: Intl.DateTimeFormatOptions = {
+    timeZone: TIME_ZONE,
+    weekday: 'long',
+};
+
+const opcoesDataNum: Intl.DateTimeFormatOptions = {
+    timeZone: TIME_ZONE,
+};
+
+export default function handleTime(horaMostrada: Date) {
+    if (!horaMostrada) return null;
+
+    const diaDaSemana = horaMostrada.toLocaleDateString(
+        'pt-BR',
+        opcoesDiaDaSemana,
+    );
+    const horas = horaMostrada.toLocaleTimeString('pt-BR', opcoesTempo);
+    const segundos = horaMostrada
+        .toLocaleTimeString('pt-BR', opcoesSegundo)
+        .padStart(2, '0');
+    const dataFormatada = horaMostrada.toLocaleDateString(
+        'pt-BR',
+        opcoesDataNum,
+    );
+
+    return { diaDaSemana, horas, segundos, dataFormatada };
+}

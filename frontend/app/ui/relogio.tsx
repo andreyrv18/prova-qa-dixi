ee02a38229794ac6d508e00f4a57f0589d1c7028
Andreyrvdev <andreyrv18@gmail.com>
2026-01-26 01:29:11 -0300

    Feature(Frontend):Logica de relógio monotomico. Evita pegar a hora do compoutador, agora mudando tanto a hora quando o fuso horario o relógio não sofre interferencia

--- frontend/app/ui/relogio.tsx
+++ frontend/app/ui/relogio.tsx
@@ -1,21 +1,94 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import { GetServerTime } from '@/app/services/fetchAPI';
 
 export default function Relogio({ children }: { children: ReactNode }) {
+    const [horaMostrada, setHoraMostrada] = useState<Date | null>(null);
+    const intevaloRef = useRef<NodeJS.Timeout | null>(null);
+    useEffect(() => {
+        const sincronizar = async () => {
+            const inicio = performance.now();
+
+            try {
+                const horaServidor = await GetServerTime();
+
+                if (!horaServidor) return;
+
+                const fim = performance.now();
+
+                const latencia = (fim - inicio) / 2;
+                const horaRealServidor = horaServidor + latencia;
+                const tempoReferenciaFrontend = performance.now();
+
+                if (intevaloRef.current) clearInterval(intevaloRef.current);
+
+                intevaloRef.current = setInterval(() => {
+                    const tempoPassado =
+                        performance.now() - tempoReferenciaFrontend;
+
+                    setHoraMostrada(new Date(horaRealServidor + tempoPassado));
+                }, 1000);
+            } catch (erro) {
+                console.error('"Erro ao obter hora do servidor"', erro);
+            }
+        };
+
+        sincronizar();
+        return () => {
+            if (intevaloRef.current) clearInterval(intevaloRef.current);
+        };
+    }, []);
+
+    if (!horaMostrada) return <p className="text-azul-base">Carregando...</p>;
+
+    const opcoesTempo: Intl.DateTimeFormatOptions = {
+        timeZone: 'America/Sao_Paulo',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    };
+
+    const opcoesSegundo: Intl.DateTimeFormatOptions = {
+        timeZone: 'America/Sao_Paulo',
+        second: '2-digit',
+    };
+
+    const opcoesDiaDaSemana: Intl.DateTimeFormatOptions = {
+        timeZone: 'America/Sao_Paulo',
+        weekday: 'long',
+    };
+
+    const opcoesDataNum: Intl.DateTimeFormatOptions = {
+        timeZone: 'America/Sao_Paulo',
+    };
+
+    const diaDaSemana = horaMostrada.toLocaleDateString(
+        'pt-BR',
+        opcoesDiaDaSemana,
+    );
+    const horas = horaMostrada.toLocaleTimeString('pt-BR', opcoesTempo);
+    const segundos = horaMostrada
+        .toLocaleTimeString('pt-BR', opcoesSegundo)
+        .padStart(2, '0');
+    const dataFormatada = horaMostrada.toLocaleDateString(
+        'pt-BR',
+        opcoesDataNum,
+    );
+
     return (
         <>
             <div className="flex flex-col gap-4">
-                <h2 className="text-subtitulo text-azul-base font-bold">
-                    Terça-Feira
+                <h2 className="text-subtitulo text-azul-base font-bold capitalize">
+                    {diaDaSemana}
                 </h2>
 
                 <h1 className="text-azul-base text-6xl font-bold">
-                    12:32
+                    {horas}
                     <span className="text-subtitulo text-corpo-de-texto font-semibold">
-                        32
+                        {segundos}
                     </span>
                 </h1>
                 <p className="text-azul-base text-subtitulo font-semibold">
-                    21/02/2026
+                    {dataFormatada}
                 </p>
 
                 {children}

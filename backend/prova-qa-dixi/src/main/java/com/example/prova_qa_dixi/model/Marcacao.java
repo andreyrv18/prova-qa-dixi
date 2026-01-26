1678d0b3a6ffa20e480089dfe6a3adaa6f676f7f
Andreyrvdev <andreyrv18@gmail.com>
2026-01-25 23:55:17 -0300

    Feature(Backend): Entidade Marcação com data, imagem, status

---
+++ backend/prova-qa-dixi/src/main/java/com/example/prova_qa_dixi/model/Marcacao.java
@@ -0,0 +1,53 @@
+package com.example.prova_qa_dixi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+
+@Entity
+public class Marcacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date dataMarcacao;
+    private String imagemSrc;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDataMarcacao() {
+        return dataMarcacao;
+    }
+
+    public void setDataMarcacao(Date dataMarcacao) {
+        this.dataMarcacao = dataMarcacao;
+    }
+
+    public String getImagemSrc() {
+        return imagemSrc;
+    }
+
+    public void setImagemSrc(String imagemSrc) {
+        this.imagemSrc = imagemSrc;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}

5ffe3a5a751d03e14bc64fd6d9ad6d530f0291fd
Andreyrvdev <andreyrv18@gmail.com>
2026-01-26 21:54:27 -0300

    fix(Backend): Controller usa de forma correta o service

--- backend/prova-qa-dixi/src/main/java/com/example/prova_qa_dixi/controller/MarcacaoController.java
+++ backend/prova-qa-dixi/src/main/java/com/example/prova_qa_dixi/controller/MarcacaoController.java
@@ -3,36 +3,37 @@ package com.example.prova_qa_dixi.controller;
 import com.example.prova_qa_dixi.model.Marcacao;
 import com.example.prova_qa_dixi.repository.MarcacaoRepository;
 import com.example.prova_qa_dixi.service.MarcacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/")
 class MarcacaoController {
 
+    @Autowired
     public MarcacaoRepository marcacaoRepository;
+    @Autowired
     public MarcacaoService marcacaoService;
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public ResponseEntity<Map<String, String>> helloWord() {
         return ResponseEntity.ok(Map.of("message", "Hello World!"));
     }
 
     @GetMapping(path = "/all")
-    public String listarMarcacoes() {
-        return marcacaoRepository.findAll().toString();
+    public Iterable<Marcacao> listarMarcacoes() {
+        return marcacaoRepository.findAll();
     }
 
 
     @PostMapping("/marcacoes")
     public ResponseEntity<Marcacao> criarMarcacao(@RequestBody Marcacao marcacao) {
 
-        Marcacao marcacaoSalva = marcacaoService.marcacaoService(marcacao);
+        Marcacao marcacaoSalva = marcacaoService.add(marcacao);
         return new ResponseEntity<>(marcacaoSalva, HttpStatus.CREATED);
     }
 }
